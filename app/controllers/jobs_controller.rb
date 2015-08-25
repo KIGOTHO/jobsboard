37f5be82656c5a8ab0c2a145522c5001578757a7
KIGOTHO <mkigotho2@gmail.com>
2015-08-25 11:49:58 +0300

    added parameter for pages

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   def index
+        @jobs = Job.page(params[:page]).per(25)
   end
 
   def new
@@ -7,5 +8,11 @@ class JobsController < ApplicationController
   end
     
     def create
+        @job = Job.new(params.require(:job).permit(:title, :company, :url))
+        if @job.save
+            redirect_to root_path
+        else
+            render "new"
+        end
     end
 end
