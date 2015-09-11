e303cba0d942f9857292214d785375dd8c0c80ec
KIGOTHO <mkigotho2@gmail.com>
2015-09-11 12:26:10 +0300

    layout all views file edit

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150825071148) do
-
+    
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.string   "company"
